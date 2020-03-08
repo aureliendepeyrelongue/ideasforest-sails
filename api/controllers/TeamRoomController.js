40eaf5df63a72fcf8fd136d016a4360b4a079ad9
MuramasaSwordMaster <aurelien.depeyrelongue@gmail.com>
2020-03-08 16:18:51 +0100

    Add api files

---
+++ api/controllers/TeamRoomController.js
@@ -0,0 +1,12 @@
+/**
+ * TeamRoomController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  
+
+};
+
